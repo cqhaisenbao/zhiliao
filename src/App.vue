d0e9514869a565605a73f9f014f47ac561ae2e40
cqhaisenbao <fanyu9353@gmail.com>
2020-11-28 14:39:51 +0800

    姓名初始化，完成顶部导航和专栏列表展示

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="container">
+        <global-header :user="currentUser"></global-header>
+        <column-list :list="list"/>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+    import {defineComponent} from 'vue';
+    import 'bootstrap/dist/css/bootstrap.min.css';
+    import ColumnList, {ColumnProps} from './components/ColumnList.vue';
+    import GlobalHeader, {UserProps} from './components/GlobalHeader.vue';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+    const currentUser: UserProps = {
+        isLogin: false,
+    };
+
+    const testData: ColumnProps[] = [
+        {
+            id: 1,
+            title: 'test1专栏',
+            description: '第一个测试专栏',
+            avatar:
+                'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e3a17c305b1070f455202.jpg?x-oss-process=image/resize,m_pad,h_50,w_50',
+        },
+        {
+            id: 2,
+            title: 'test2专栏',
+            description: '第二个测试专栏',
+        },
+        {
+            id: 3,
+            title: 'test3专栏',
+            description: '第三个测试专栏,第三个测试专栏,第三个测试专栏,第三个测试专栏',
+            avatar:
+                'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e3a17c305b1070f455202.jpg?x-oss-process=image/resize,m_pad,h_50,w_50',
+        },
+        {
+            id: 4,
+            title: 'test4专栏',
+            description:
+                '第四个测试专栏,第四个测试专栏第四个测试专栏第四个测试专栏第四个测试专栏',
+            avatar:
+                'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e3a17c305b1070f455202.jpg?x-oss-process=image/resize,m_pad,h_50,w_50',
+        },
+    ];
+
+    export default defineComponent({
+        name: 'App',
+        components: {ColumnList, GlobalHeader},
+        setup() {
+            return {
+                list: testData,
+                currentUser,
+            };
+        },
+    });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
